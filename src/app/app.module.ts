667f3ec3718a00d2602166d580284a0ef530026e
Mitchell Lu <Mitchell.Lu@xpo.com>
2017-09-26 16:05:35 -0700

    Initial Commit of Angular 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchUsersComponent } from './search-users/search-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUsersComponent
   ],
   imports: [
     BrowserModule
